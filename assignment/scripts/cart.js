40c99cc3b8e25a7a79468309ce236484ba23b725
Kas Eam <KassandraEam@gmail.com>
2022-07-01 13:23:44 -0500

    Completed Stretch goals 1 and 2.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,15 +1,21 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+//Required Features:
 let basket = [];
 addItem = (item) =>{
     basket.push(item);
-    console.log(basket);
+    //console.log(`${item} was added to the basket. The basket is now:`, basket);
     return true;
 };
 
 addItem('T-shirt');
 addItem('Dress pants');
+addItem('Belt');
+addItem('Shoes');
+addItem('Second pair of shoes');
+addItem('Tank top');
 
 listItems = () => {
     for (let i=0; i<basket.length; i++) {
@@ -19,9 +25,27 @@ listItems = () => {
 
 listItems();
 
-empty = () => {
+empty = () => { 
     basket = [];
     //console.log(basket);
 };
 
-empty();
+//empty();
+//listItems();
+//Stretch Goals
+
+let maxItems = 5;
+
+isFull = () => {
+    //false if basket.length < maxItems.
+    //true if basket.length >= maxItems.
+    if (basket.length < maxItems){
+        console.log('Basket has room.');
+        return true;
+    } else if (basket.length >= maxItems){
+        console.log('Basket is full.');
+        return false;
+    }
+};
+
+//isFull();
