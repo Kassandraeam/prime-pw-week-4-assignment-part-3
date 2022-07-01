14e45d52e35c9ed091ef0fd6b30cc91a1353a41e
Kas Eam <KassandraEam@gmail.com>
2022-07-01 13:11:39 -0500

    Completetd Required Features.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,27 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+addItem = (item) =>{
+    basket.push(item);
+    console.log(basket);
+    return true;
+};
+
+addItem('T-shirt');
+addItem('Dress pants');
+
+listItems = () => {
+    for (let i=0; i<basket.length; i++) {
+        console.log(basket[i]);
+    }
+};
+
+listItems();
+
+empty = () => {
+    basket = [];
+    //console.log(basket);
+};
+
+empty();
